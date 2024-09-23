18c40c389195ce6ccb4ee13d5d0fcf5e29a2abe4
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-09-23 10:54:08 +0700

    deleted:    .gitignore 	deleted:    Video_to_Frames_Extractor.egg-info/PKG-INFO 	deleted:    Video_to_Frames_Extractor.egg-info/SOURCES.txt 	deleted:    Video_to_Frames_Extractor.egg-info/dependency_links.txt 	deleted:    Video_to_Frames_Extractor.egg-info/top_level.txt 	deleted:    demo.py 	deleted:    notebook/experiments.ipynb 	deleted:    pipeline/__pycache__/json_video_to_sequence_pipeline.cpython-39.pyc 	deleted:    pipeline/__pycache__/video_to_sequences_pipeline.cpython-39.pyc 	deleted:    pipeline/__pycache__/xml_video_to_sequence_pipeline.cpython-39.pyc 	deleted:    pipeline/__pycache__/yolo_video_to_sequence_pipeline.cpython-39.pyc 	deleted:    pipeline/json_video_to_sequence_pipeline.py 	deleted:    pipeline/xml_video_to_sequence_pipeline.py 	deleted:    pipeline/yolo_video_to_sequence_pipeline.py 	deleted:    requirements.txt 	deleted:    run_pipeline.py 	deleted:    setup.py 	deleted:    src/__pycache__/get_file_names_from_txt.cpython-39.pyc 	deleted:    src/__pycache__/video_to_frames.cpython-39.pyc 	deleted:    src/coco_src/__pycache__/coco_output_setup_directory_structure.cpython-39.pyc 	deleted:    src/coco_src/__pycache__/modify_json_file.cpython-39.pyc 	deleted:    src/coco_src/coco_output_setup_directory_structure.py 	deleted:    src/coco_src/modify_json_file.py 	deleted:    src/get_file_names_from_txt.py 	deleted:    src/pascal_xml_src/__pycache__/modify_xml_file.cpython-39.pyc 	deleted:    src/pascal_xml_src/__pycache__/setup_directory_structure_xml_output.cpython-39.pyc 	deleted:    src/pascal_xml_src/__pycache__/update_xml.cpython-39.pyc 	deleted:    src/pascal_xml_src/__pycache__/xml_output_setup_directory_structure.cpython-39.pyc 	deleted:    src/pascal_xml_src/modify_xml_file.py 	deleted:    src/pascal_xml_src/update_xml.py 	deleted:    src/pascal_xml_src/xml_output_setup_directory_structure.py 	deleted:    src/video_to_frames.py 	deleted:    src/yolo_src/__pycache__/extract_classes_from_data_yaml.cpython-39.pyc 	deleted:    src/yolo_src/__pycache__/get_file_names_from_txt.cpython-39.pyc 	deleted:    src/yolo_src/__pycache__/setup_directory_structure.cpython-39.pyc 	deleted:    src/yolo_src/__pycache__/update_yaml.cpython-39.pyc 	deleted:    src/yolo_src/__pycache__/video_to_frames.cpython-39.pyc 	deleted:    src/yolo_src/extract_classes_from_data_yaml.py 	deleted:    src/yolo_src/setup_directory_structure.py 	deleted:    src/yolo_src/update_yaml.py
    Untracked files:
    	.gitignore
    	demo.py
    	notebook/
    	pipeline/
    	requirements.txt
    	run_pipeline.py
    	setup.py
    	src/

--- demo.py
+++
@@ -1,9 +0,0 @@
-import json
-
-json_path = 'port/annotations/person_keypoints_default.json'
-
-with open(json_path, 'r') as file:
-    data = json.load(file)
-
-with open(json_path, 'w') as f:
-    json.dump(data, f, indent=4)
