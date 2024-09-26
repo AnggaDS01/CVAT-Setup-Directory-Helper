8b75cef443b0f3bf5096a77c7ed251b49a7197dd
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-09-26 12:31:35 +0700

    modified:   run_pipeline.py
    Changes not staged for commit:
    	deleted:    pipeline/coco_json_video_to_sequence_pipeline.py
    	deleted:    pipeline/pacal_voc_xml_video_to_sequence_pipeline.py
    	deleted:    pipeline/yolo_video_to_sequence_pipeline.py
    	modified:   run_plot_results/plot_coco_json_bbox_result.py
    	modified:   run_plot_results/plot_pascal_voc_bbox_result.py
    	modified:   run_plot_results/plot_yolo_bbox_result.py
    	deleted:    src/coco_src/coco_json_splitter.py
    	deleted:    src/coco_src/modify_json_file.py
    	deleted:    src/get_file_names_from_txt.py
    	deleted:    src/pascal_xml_src/modify_xml_file.py
    	deleted:    src/pascal_xml_src/pascal_voc_setup_directory_structure.py
    	deleted:    src/pascal_xml_src/pascal_voc_splitter.py
    	deleted:    src/pascal_xml_src/update_xml.py
    	deleted:    src/plot_results/plot_coco_json_bbox.py
    	deleted:    src/plot_results/plot_pascal_voc_bbox.py
    	deleted:    src/plot_results/plot_yolo_bbox.py
    	deleted:    src/video_to_frames.py
    	deleted:    src/yolo_src/extract_classes_from_data_yaml.py
    	deleted:    src/yolo_src/update_yaml.py
    	deleted:    src/yolo_src/yolo_setup_directory_structure.py
    	deleted:    src/yolo_src/yolo_splitter.py
    	modified:   tree.py
    	modified:   tree_output.txt
    
    Untracked files:
    	src/components/
    	src/constants/
    	src/pipeline/

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,20 +1,39 @@
 # Penggunaan pipeline
-from pipeline.yolo_video_to_sequence_pipeline import yolo_process_video_pipeline
-from pipeline.pacal_voc_xml_video_to_sequence_pipeline import xml_process_video_pipeline
-from pipeline.coco_json_video_to_sequence_pipeline import coco_json_process_video_pipeline
+from src.pipeline.yolo_video_to_sequence_pipeline import yolo_process_video_pipeline
+from src.pipeline.pacal_voc_xml_video_to_sequence_pipeline import xml_process_video_pipeline
+from src.pipeline.coco_json_video_to_sequence_pipeline import coco_json_process_video_pipeline
+from src.constants import *
 
-import os
+# yolo_process_video_pipeline(
+#     project_path=PROJECT_PATH, 
+#     source_filename=FILE_NAMES, 
+#     video_path=VIDEO_PATH, 
+#     project_name=PROJECT_NAME, 
+#     split_ratio=SPLIT_RATIO,
+#     random_split=RANDOM_SPLIT,
+#     seed=SEED,
+#     is_split=IS_SPLIT,
+#     ext=EXT
+# )
 
-GRANDPARENT_NAME = 'Data Zone' # Put your data in this folder
-PARENT_NAME = 'Traffic Assets' # Change this
-PROJECT_NAME = 'traffic-YOLOv8' # Change this
-VIDEO_NAME = '4K Video of Highway Traffic! (online-video-cutter.com).mp4' # Change this
-FILE_NAMES = 'train.txt'
+# xml_process_video_pipeline(
+#     project_path=PROJECT_PATH, 
+#     source_filename=FILE_NAMES, 
+#     video_path=VIDEO_PATH, 
+#     split_ratio=SPLIT_RATIO, 
+#     random_split=RANDOM_SPLIT,
+#     seed=SEED,
+#     is_split=IS_SPLIT,
+#     ext=EXT
+# )
 
-BASE_DIR = os.path.join(GRANDPARENT_NAME, PARENT_NAME)
-PROJECT_PATH = os.path.join(BASE_DIR, PROJECT_NAME)
-VIDEO_PATH = os.path.join(BASE_DIR, VIDEO_NAME)
-
-yolo_process_video_pipeline(PROJECT_PATH, FILE_NAMES, VIDEO_PATH, PROJECT_NAME, split_ratio=0.8, random_split=True, is_split=True)
-# xml_process_video_pipeline(PROJECT_PATH, FILE_NAMES, VIDEO_PATH, split_ratio=0.8, random_split=True, is_split=True)
-# coco_json_process_video_pipeline(PROJECT_PATH, FILE_NAMES, VIDEO_PATH, is_split=True, random_split=True, split_ratio=0.9)
+coco_json_process_video_pipeline(
+    project_path=PROJECT_PATH, 
+    coco_json_filename=FILE_NAMES, 
+    video_path=VIDEO_PATH, 
+    split_ratio=SPLIT_RATIO,
+    random_split=RANDOM_SPLIT, 
+    is_split=IS_SPLIT, 
+    seed=SEED,
+    ext=EXT
+)
